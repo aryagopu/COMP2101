26a821494a1e6796a5fd1eb1c2acef9b6b191eaf
aryagopu <46567605+aryagopu@users.noreply.github.com>
2020-02-04 15:22:21 -0500

    Add files via upload

---
+++ passwordguesser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script demonstrates testing to see if 2 strings are the same
+# it also demonstrates using conditional execution
+
+# TASK 1: Improve it by asking the user for a password guess instead of using inline literal data (i.e. TestString)
+# TASK 2: Improve it by rewriting it to use the if command
+#         The if command should test if they got it right, in which case it should tell them they got it right
+#         The if command should tell them they got it wrong
+# TASK 3: Improve it by giving them 5 tries to get it right before failing
+#           *** Do not use the exit command, and do not use a loop (e.g. while or for)
+
+myString="TestString"
+referenceString="password"
+for try in {1..5}
+do
+	read -p "Try ($try): Enter a password guess:" myString
+	if [ $myString = $referenceString ]
+	then
+		printf "Congratulations your guess is right!\n"
+		break
+	else
+		echo "The password you entered  is wrong..."
+	fi
+done
